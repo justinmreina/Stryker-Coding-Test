cb3de23767ae1ae8d02dcc9ec26c24d66c70c150
Justin Reina <justinmreina@gmail.com>
2024-01-25 08:37:04 -0500

    (+)    initial commit

---
+++ Ref/stdcomments.h
@@ -0,0 +1,77 @@
+/******************************************************************************/
+/** @file		x
+ * 	@brief		x
+ * 	@details	x
+ *
+ * 	@author		Justin Reina, Firmware Engineer
+ * 	@created	1/25/24
+ * 	@last rev	1/25/24
+ *
+ *
+ * 	@notes		x
+ *
+ * 	@section	Opens
+ * 			none current
+ *
+ * 	@section	Legal Disclaimer
+ * 			© 2025 Justin Reina, All rights reserved. All contents of this
+ * 			source file and/or any other related source files are the explicit
+ * 			property of Justin Reina. Do not distribute. Do not copy.
+ */
+/******************************************************************************/
+
+
+/******************************************************************************/
+/**	@fcn		int main(void)
+ *  @brief		x
+ *  @details	x
+ *
+ *  @section	Purpose
+ *  	x
+ *
+ *  @param		[in]	name	descrip
+ *
+ *  @param		[out]	name	descrip
+ *
+ *  @return		(type) descrip
+ *
+ *  @pre		x
+ *
+ *  @post		x
+ *
+ *  @section	Operation
+ *		x
+ *		
+ *  @section	Opens
+ *  	x
+ *
+ *  @section	Hazards & Risks
+ *  	x
+ *
+ *	@section	Todo
+ *		x
+ *
+ *  @section	Timing
+ *  	x
+ *
+ *  @note		x
+ */
+/******************************************************************************/
+
+
+//----------------------------- A Delimiting Note ----------------------------//
+
+//****************************************************************************//
+//		 						 SECTION HEADER								  //
+//****************************************************************************//
+
+												/*	note to the side		  */
+
+// a note on top
+// that spans multiple lines
+
+// PROTOTYPES, DECLARATIONS AND PREPROC --------------------------------------//
+
+//[1/x]----------------------------- STEP 1 ----------------------------------//
+
+
---
+++ readme.txt
@@ -0,0 +1,12 @@
+@brief 		Stryker C++ Coding Test
+@details 	x
+@auth 		Justin Reina
+@date 		1/25/24
+
+
+@section 	Notes
+	x
+
+@section 	Restrictions
+	Content shared here is public for review
+
